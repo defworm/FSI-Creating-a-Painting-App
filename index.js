874f067e0757d0197afdfe98e25b7fe379c593cc
Crystal Korth <crystalkorth@gmail.com>
2022-04-06 21:25:49 -0500

    changed to for loop

--- index.js
+++ index.js
@@ -4,26 +4,10 @@ painting.addEventListener('click', function(e){
     e.target.style.backgroundColor = selectedColor
 })
 
-let blueBox = document.querySelector("#blue")
-
-blueBox.addEventListener('click', function(){
-selectedColor = 'blue'
-})
-
-let greenBox = document.querySelector('#green')
-
-greenBox.addEventListener('click', function(){
-    selectedColor = 'green'
-})
-
-let redBox = document.querySelector('#red')
-
-    redBox.addEventListener('click', function(){
-        selectedColor = 'red'
-})
-
-let yellowBox = document.querySelector('#yellow')
-
-yellowBox.addEventListener('click', function(){
-    selectedColor = 'yellow'
-})
+let colorBoxes = document.querySelectorAll(".color-choice")
+console.log(".........colorBoxes", colorBoxes)
+for(let i = 0; i < colorBoxes.length; i++){
+    colorBoxes[i].addEventListener('click', function(){
+        selectedColor = colorBoxes[i].id
+    })
+}
