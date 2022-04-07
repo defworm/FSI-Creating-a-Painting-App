d745cd79f4515be8329c9675048ef67eaf5c4580
Crystal Korth <crystalkorth@gmail.com>
2022-04-06 20:52:35 -0500

    blue dots are working, can change selectedColor to red or whatever

---
+++ index.js
@@ -0,0 +1,5 @@
+let selectedColor = 'blue'
+const painting = document.querySelector('.painting')
+painting.addEventListener('click', function(e){
+    e.target.style.backgroundColor = selectedColor
+})
