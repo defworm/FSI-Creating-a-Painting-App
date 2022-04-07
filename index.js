5f5095ba96f7a68893a98f8ff6a54ec10dba981c
Crystal Korth <crystalkorth@gmail.com>
2022-04-06 21:15:26 -0500

    added four color event listeners

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-let selectedColor = 'blue'
+let selectedColor = 'red'
 const painting = document.querySelector('.painting')
 painting.addEventListener('click', function(e){
     e.target.style.backgroundColor = selectedColor
+})
+
+let blueBox = document.querySelector("#blue")
+
+blueBox.addEventListener('click', function(){
+selectedColor = 'blue'
+})
+
+let greenBox = document.querySelector('#green')
+
+greenBox.addEventListener('click', function(){
+    selectedColor = 'green'
+})
+
+let redBox = document.querySelector('#red')
+
+    redBox.addEventListener('click', function(){
+        selectedColor = 'red'
+})
+
+let yellowBox = document.querySelector('#yellow')
+
+yellowBox.addEventListener('click', function(){
+    selectedColor = 'yellow'
 })
